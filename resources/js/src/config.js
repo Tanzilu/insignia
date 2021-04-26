d6316cb6a9409a968e83a243572b8b43d5604224
Tanzilu <akmaltanzilu@gmail.com>
2021-04-26 14:45:29 +0700

    fix bug

--- resources/js/src/config.js
+++ resources/js/src/config.js
@@ -1,14 +1,10 @@
 const protocol = window.location.protocol
 const hostname = `${protocol  }//${  window.location.hostname}`
 
-window.localserver = '' /**Please set local path on development mode */
+window.localserver = ':8000'
 
 if (process.env.NODE_ENV === 'production') {
-  // mutate config for production...
   window.url = `${hostname  }/`
-  // window.imgUrl = hostname+'/storage/';
 } else {
-  // mutate for development...
   window.url = `${hostname + localserver  }/`
-  // window.imgUrl = hostname+localserver+'/storage/';
 }
